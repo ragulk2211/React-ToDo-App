54c927626ed537e0de979bc5c958520fe099f1b4
ragulk2211 <ragulk0095@gmail.com>
2026-07-18 16:23:23 +0530

    Fix import order in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
-import axios from "axios";
-
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || "";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios';
 // import * as serviceWorker from './serviceWorker';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || "";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // serviceWorker.register();
